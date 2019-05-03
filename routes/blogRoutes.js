09a08cb5925c5e6d080d6d9b51190739d8ea79cb
DeanDK <deanbozic12@gmail.com>
2019-05-03 21:47:58 +0200

    initial redis check

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,13 +13,30 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const redis = require("redis");
+    const redisUrl = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redisUrl);
+    const util = require("util");
 
+    // passing reference of client.get function to in order to use promises (await)
+    client.get = util.promisify(client.get);
+
+    // Is there any cached data in redis related to query ?
+    const cachedBlogs = await client.get(req.user.id);
+    // If true, respond right away
+    if (cachedBlogs) {
+      console.log("SERVING FROM CACHE");
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    // else respond to request and update our cache to store the data
+    console.log("SERVING FROM mongodb");
+    const blogs = await Blog.find({ _user: req.user.id });
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
