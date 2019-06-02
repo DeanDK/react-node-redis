3ad10200efaa93f1b880a2034a12a723164895d4
DeanDK <deanbozic12@gmail.com>
2019-06-02 10:40:45 +0200

    added small test to check whether login button redirects to google accounts page

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,16 +1,33 @@
 const puppeteer = require('puppeteer');
 
-test('Adds two numbers', () => {
-  const sum = 1 + 2;
+let browser, page;
 
-  expect(sum).toEqual(3);
+beforeEach(async () => {
+  // open browser
+  browser = await puppeteer.launch({  headless: false  });
+
+  // open empty tab
+  page = await browser.newPage();
+
+  // navigate to localhost
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
 })
 
-test('We can launch a browser', async () => {
-  // open browser
-  const browser = await puppeteer.launch({
-    headless: false
-  });
-  // open single page
-  const page = await browser.newPage();
+test('the header has the correct text', async () => {
+  // fetch the text of DOM element
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+})
+
+test('clicking login starts oauth flow', async() => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
 })
